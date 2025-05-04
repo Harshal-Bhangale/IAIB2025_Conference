9623059cf59fd9cbf0439173ea61971978c4165a
Harshal Bhangale <99475142+Harshal-Bhangale@users.noreply.github.com>
2025-05-04 12:05:26 +0530

    Update

--- src/Components/ImportantDate.jsx
+++ src/Components/ImportantDate.jsx
@@ -17,12 +17,12 @@ const ImportantDates = () => {
 
                     <div className="flex justify-between items-center">
                         <span className="text-xl font-medium text-gray-700">Acceptance Notification:</span>
-                        <span className="text-xl text-gray-500">30th May, 2025</span>
+                        <span className="text-xl text-gray-500">10th May, 2025</span>
                     </div>
 
                     <div className="flex justify-between items-center">
                         <span className="text-xl font-medium text-gray-700">Registration & Camera Ready Paper Submission:</span>
-                        <span className="text-xl text-gray-500">10th May, 2025</span>
+                        <span className="text-xl text-gray-500">20th May, 2025</span>
                     </div>
                 </div>
             </div>
